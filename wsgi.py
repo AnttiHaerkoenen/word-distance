73d2f055a846f06c4abe3f91b04e99309ca9f9c5
AnttiHaerkoenen <antth@hotmail.fi>
2020-04-20 13:35:55 +0300

    2020.0 (2)

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
+from dash.exceptions import PreventUpdate
 import pandas as pd
+
 
 VERSION = 2020.0
 data_dir = 'https://raw.githubusercontent.com/AnttiHaerkoenen/grand_duchy/master/data/processed/'
@@ -13,31 +15,54 @@ languages = {
     # 'English': 'en',
 }
 
+
+def get_distances(
+        language,
+        kw1,
+        kw2,
+        data,
+):
+    if not kw1 or not kw2:
+        return None, None
+
+    lang_data = data.get(language)
+
+    x = []
+    y = []
+
+    for year, m in lang_data.items():
+        if kw1 in m.index and kw2 in m.index:
+            y.append(m.loc[kw1, kw2])
+            x.append(year)
+
+    return x, y
+
+
 distance_data = {
     'fi': {
-        year: pd.read_csv(data_dir + f'distance_matrix_fi_{year}.csv')
+        year: pd.read_csv(data_dir + f'distance_matrix_fi_{year}.csv', index_col=0)
         for year
         in range(1820, 1881, 20)
     },
     'sv': {
-        year: pd.read_csv(data_dir + f'distance_matrix_sv_{year}.csv')
+        year: pd.read_csv(data_dir + f'distance_matrix_sv_{year}.csv', index_col=0)
         for year
         in range(1740, 1901, 20)
     },
     # 'en': {
-    #     year: pd.read_csv(data_dir + f'distance_matrix_en_{year}.csv')
+    #     year: pd.read_csv(data_dir + f'distance_matrix_en_{year}.csv', index_col=0)
     #     for year
     #     in range(1820, 1881, 20)
     # }
 }
 
-keywords = {
-    l: {
+keyword_options = {
+    lang: [''] + sorted({
         kw
         for year_data in lang_data.values()
         for kw in year_data.index
-    }
-    for (l, lang_data)
+    })
+    for (lang, lang_data)
     in distance_data.items()
 }
 
@@ -71,8 +96,28 @@ app.layout = html.Div(children=[
 
         dcc.Dropdown(
             id='keyword-picker',
-            options=keywords,
-            value=keywords[0]['value'],
+        ),
+    ]),
+
+    html.Div([
+        html.H2(children='Select comparison keywords'),
+
+        html.H4(children='Keyword 1'),
+
+        dcc.Dropdown(
+            id='keyword-picker-1',
+        ),
+
+        html.H4(children='Keyword 2'),
+
+        dcc.Dropdown(
+            id='keyword-picker-2',
+        ),
+
+        html.H4(children='Keyword 3'),
+
+        dcc.Dropdown(
+            id='keyword-picker-3',
         ),
     ]),
 
@@ -88,30 +133,95 @@ app.layout = html.Div(children=[
 
 
 @app.callback(
+    Output('keyword-picker', 'options'),
+    [
+        Input('language-picker', 'value'),
+    ]
+)
+def set_keyword_options(
+        language,
+):
+    kw_options = [
+        {'label': kw, 'value': kw}
+        for kw
+        in keyword_options.get(language, ())
+    ]
+    return kw_options
+
+
+@app.callback(
+    [
+        Output('keyword-picker-1', 'options'),
+        Output('keyword-picker-2', 'options'),
+        Output('keyword-picker-3', 'options'),
+    ],
+    [
+        Input('language-picker', 'value'),
+        Input('keyword-picker', 'value'),
+    ]
+)
+def set_other_keyword_options(
+        language,
+        first_keyword,
+):
+    updated_kws = keyword_options.get(language, ())
+
+    if first_keyword in updated_kws:
+        updated_kws.remove(first_keyword)
+
+    updated_kw_options = [
+        {'label': kw, 'value': kw}
+        for kw
+        in updated_kws
+    ]
+
+    return updated_kw_options, updated_kw_options, updated_kw_options
+
+
+@app.callback(
     Output('line-plot', 'figure'),
-    [Input('keyword-picker', 'value'),
-     Input('abs-picker', 'value'),]
+    [
+        Input('language-picker', 'value'),
+        Input('keyword-picker', 'value'),
+        Input('keyword-picker-1', 'value'),
+        Input('keyword-picker-2', 'value'),
+        Input('keyword-picker-3', 'value'),
+    ]
 )
 def update_graph(
+        language,
         keyword,
-        abs_or_rel,
+        *other_keywords,
 ):
-    x = data['year']
-    y = data[keyword]
+    if not keyword:
+        raise PreventUpdate
+
+    data = []
+
+    for kw in other_keywords:
+        x, y = get_distances(
+            language,
+            keyword,
+            kw,
+            distance_data,
+        )
+
+        data.append(
+            {
+                'x': x,
+                'y': y,
+                'name': kw,
+            }
+        )
 
     return {
-        'data': [{
-            'x': x,
-            'y': y,
-            'type': 'line',
-            'name': keyword,
-        }]
+        'data': data,
     }
 
 
 if __name__ == '__main__':
     app.run_server(
         port=8080,
-        # host='0.0.0.0',
+        host='0.0.0.0',
         debug=True,
     )
